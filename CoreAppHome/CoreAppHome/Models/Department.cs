a5041fd9a594715bd877ac314157d3e94f28e5e7
maheshsabnis <sabnis_m@hotmail.com>
2019-06-29 11:20:31 +0530

    Model Classes

---
+++ CoreAppHome/CoreAppHome/Models/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace CoreAppHome.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DeptRowId { get; set; }
+        [Required(ErrorMessage ="DeptNo is required")]
+        public string DeptNo { get; set; }
+        [Required(ErrorMessage = "DeptName is required")]
+        public string DeptName { get; set; }
+        [Required(ErrorMessage = "Location is required")]
+        public string Location { get; set; }
+        [Required(ErrorMessage = "Capacity is required")]
+        public int Capacity { get; set; }
+        // one-to-many relationship
+        public ICollection<Employee> Employees { get; set; }
+    }
+
+    
+}
---
+++ CoreAppHome/CoreAppHome/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace CoreAppHome.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpRowId { get; set; }
+        [Required(ErrorMessage ="EmpNo is required")]
+        public string EmpNo { get; set; }
+        [Required(ErrorMessage = "EmpName is required")]
+        public string EmpName { get; set; }
+        [Required(ErrorMessage = "Designation is required")]
+        public string Designation { get; set; }
+        [Required(ErrorMessage = "Salary is required")]
+        public int Salary { get; set; }
+        [Required(ErrorMessage = "DeptRowId is required")]
+        public int DeptRowId { get; set; } // foreign key
+        public Department Department { get; set; } // referencial integrity
+    }
+}
